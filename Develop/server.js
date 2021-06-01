dd6defe696b1635ddc79cdc84d93798efcb1723e
BRASS <mr.brn.lewis@outlook.com>
2021-06-01 23:35:22 +0800

    server loads and delivers the HTML pages

--- Develop/server.js
+++ Develop/server.js
@@ -9,12 +9,24 @@ const PORT = 8080;
 
 expressServer.get('/notes',(req,res) => 
 {
-    fs.readFile('./notes.html',{ 'content-type':'text/html' },(err,notes) => 
+    fs.readFile('./public/notes.html',{ 'content-type':'text/html' },(err,notes) => 
     {
         if (err) return err;
-        res.send(notes);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(notes);
     });
 });
+
+expressServer.get('*',(req,res) => 
+{
+    fs.readFile('./public/index.html',{ 'content-type':'text/html' },(err,home) => 
+    {
+        if (err) return err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(home);
+    });
+});
+
 
 expressServer.listen(PORT,() => {console.log("listening on Port: ", PORT)});
 
