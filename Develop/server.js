50a031e9e761aa083e7bf23308055f14e8bf35dd
BRASS <mr.brn.lewis@outlook.com>
2021-06-02 00:32:53 +0800

    add api/notes to return json working in basic form

--- Develop/server.js
+++ Develop/server.js
@@ -2,14 +2,17 @@
 //import modules
 const express = require('express');
 const fs = require('fs');
+const { type } = require('os');
 
-// assign express object to app;
+// assign express object to expressServer;
 const expressServer = express();
 const PORT = 8080;
 
+//create get listeners
+
 expressServer.get('/notes',(req,res) => 
 {
-    fs.readFile('./public/notes.html',{ 'content-type':'text/html' },(err,notes) => 
+    fs.readFile('./public/notes.html',(err,notes) => 
     {
         if (err) return err;
         res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -17,17 +20,30 @@ expressServer.get('/notes',(req,res) =>
     });
 });
 
-expressServer.get('*',(req,res) => 
+
+expressServer.get('/api/notes',(req,res) => 
 {
-    fs.readFile('./public/index.html',{ 'content-type':'text/html' },(err,home) => 
+    fs.readFile('./db/db.json',{'content-type':'application/json'},(err,data) => 
     {
         if (err) return err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(data);
+    });
+
+});
+
+expressServer.get('*',(req,res) => 
+{
+    fs.readFile('./public/index.html',{'content-type':'text/html'},(err,home) => 
+    {
+        if (err) return err;
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(home);
     });
 });
 
 
+//GET /api/notes should read the db.json file and return all saved notes as JSON.
 expressServer.listen(PORT,() => {console.log("listening on Port: ", PORT)});
 
 
